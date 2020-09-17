bac9ea86f63f29f9a82d240b769afaf45b4259c0
kimdoori <dudu@spoqa.com>
2020-09-17 19:45:12 +0900

    Add sample module

---
+++ sample/src/main/java/com/spoqa/sample/views/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.spoqa.sample.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spoqa.sample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
